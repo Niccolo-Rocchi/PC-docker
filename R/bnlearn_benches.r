429848923824bcede8b77c97fb871bd074343dad
Niccolo-Rocchi <nik9991u@gmail.com>
2023-06-22 19:16:32 +0200

    Add parallel benchmarking of R `pc.stable`

--- R/bnlearn_benches.r
+++ R/bnlearn_benches.r
@@ -1,5 +1,6 @@
 library(bnlearn)
 library(microbenchmark)
+library(parallel)
 
 # Read file names
 csv <- list.files(
@@ -12,12 +13,19 @@ csv <- list.files(
 names <- c()
 benches <- data.frame()
 for (df in csv) {
+    # Get dataframe
     name <- strsplit(strsplit(df, split = "//")[[1]][2], split=".csv")[[1]][1]
     names <- c(names, name)
     df <- read.csv(df)[-1] |> lapply(factor) |> data.frame()
-    mbm <- microbenchmark(pc.stable(df, alpha = 0.05), times = 100L) |> data.frame()
+    # Single core benches
+    mbm <- microbenchmark(pc.stable(x = df, alpha = 0.05), times = 100L) |> data.frame()
     mbm$expr <- name
     benches <- rbind(benches, mbm)
+    # Multi core benches
+    fork_cluster <- makeForkCluster(nnodes = detectCores())
+    par_mbm <- microbenchmark(pc.stable(x= df, cluster = fork_cluster, alpha = 0.05), times = 100L) |> data.frame()
+    par_mbm$expr <- paste0("par_", name)
+    benches <- rbind(benches, par_mbm)
 }
 
 # Save benches results
